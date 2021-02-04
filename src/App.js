44df7834aee9a3e125077a08b20dad561df2b314
johng <jcguest90@gmail.com>
2021-02-04 16:48:37 -0600

    create TodoItem component, map through dummy todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
 import Input from './components/Input.js';
+import TodoItem from './components/TodoItem';
+
+const todoList = [
+  {
+    item: 'todo',
+    done: false,
+    id: 123456
+  },
+  {
+    item: 'todo1',
+    done: true,
+    id: 12345635
+  }
+];
 
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
+      <div className='app__container'>
+        <div className='app__todoContainer'>
+          {todoList.map((item) => (
+            <TodoItem name={item.item} done={item.done} id={item.id} />
+          ))}
+        </div>
+      </div>
       <Input />
     </div>
   );
---
+++ src/components/TodoItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './TodoItem.css  ';
+
+const TodoItem = (name, done, id) => {
+  return <div className='todoItem'></div>;
+};
+
+export default TodoItem;
