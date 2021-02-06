b11553803bbbab1ca92187808c32f7cd1e355588
johng <jcguest90@gmail.com>
2021-02-05 23:43:28 -0600

    connect state to app with hook

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from 'react';
 import './App.css';
 import Input from './components/Input.js';
 import TodoItem from './components/TodoItem';
+import { useSelector } from 'react-redux';
+import { selectTodoList } from './features/todoReducer';
 
-const todoList = [
-  {
-    item: 'todo',
-    done: false,
-    id: 123456
-  },
-  {
-    item: 'todo1',
-    done: true,
-    id: 12345635
-  }
-];
+// const todoList = [
+//   {
+//     item: 'todo',
+//     done: false,
+//     id: 123456
+//   },
+//   {
+//     item: 'todo1',
+//     done: true,
+//     id: 12345635
+//   }
+// ];
 
 function App() {
+  const todoList = useSelector(selectTodoList);
   return (
     <div className='app'>
       <div className='app__container'>
